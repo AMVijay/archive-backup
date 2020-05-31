fb885f70eb23b99fd22c1b54124b5cb27fb511da
Vijay <AM.Vijay@gmail.com>
2020-05-31 14:51:22 -0500

    Python Code to create archive and split files.

---
+++ python/archiveBackup.py
@@ -0,0 +1,25 @@
+import tarfile
+import os
+
+# tar = tarfile.open("sample.tar.gz","w:gz")
+tar = tarfile.open("sample.tar","w")
+tar.add("C:\\Users\\135670\\Vijay\\poc-code\\amvijay.github.io")
+tar.close
+
+CHUNK_SIZE = 500000
+file_number = 1
+with open("sample.tar", mode="rb") as f:
+    chunk = f.read(CHUNK_SIZE)
+    while chunk:
+        with open("sample.tar" + str(file_number), mode="wb") as chunk_file:
+            chunk_file.write(chunk)
+        file_number += 1
+        chunk = f.read(CHUNK_SIZE)
+
+file_number = 1
+with open("sample-merged.tar",mode="ab") as merged_file:
+    while os.path.exists("sample.tar"+str(file_number)):
+        print("sample.tar"+str(file_number) + "reading the file")
+        with open("sample.tar" + str(file_number), mode="rb") as chunk_file:
+            merged_file.write(chunk_file.read())
+            file_number = file_number + 1
